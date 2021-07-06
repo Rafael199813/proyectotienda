91c5ffa881b877bf17f506dcdeb073a57d28fb95
Rafael199813 <sasuke201002@hotmail.com>
2021-07-05 19:30:25 -0500

    ultima

--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,12 @@ router.get('/home', async (req, res) => {
         tasks
     });
 });
+router.get('/admin', async (req, res) => {
+    const tasks = await Usu.find();
+    res.render('admin', {
+        tasks
+    });
+});
 router.get('/categoria/:id', async (req, res) => {
     const { id } = req.params;
     const tasks = await Task.find({categoria: id });
@@ -47,6 +53,12 @@ router.get('/ubicacion', async (req, res) => {
         tasks
     });
 });
+router.get('/agregarusu', async (req, res) => {
+    const task_usuario = await Usu.find();
+    res.render('agragar_usu', {
+        task_usuario
+    });
+});
 router.get('/detalles/:id', async (req, res) => {
     const { id } = req.params;
     const task = await Task.findById(id); res.render('det', {
@@ -63,10 +75,11 @@ router.post('/add', async (req, res) => {
     await task.save();
     res.redirect('/');
 });
-router.post('/add2', async (req, res) => {
-    const task = new Usu(req.body);
-    await task.save();
-    res.redirect('/usu');
+
+router.post('/add3', async (req, res) => {
+    const task_usuario = new Usu(req.body);
+    await task_usuario.save();
+    res.redirect('/agregarusu');
 });
 router.get('/turn/:id', async (req, res) => {
     const { id } = req.params;
@@ -84,8 +97,21 @@ router.get('/edit/:id', async (req, res) => {
 });
 
 router.post('/edit/:id', async (req, res) => {
-    const { id } = req.params; await Task.updateMany({ _id: id }, req.body);
+    const { id } = req.params; 
+    const task = await Task.updateMany({ _id: id }, req.body);
     res.redirect('/');
+});
+router.get('/edit1/:id', async (req, res) => {
+    const { id } = req.params;
+    const task_usuario = await Usu.findById(id); res.render('edit1', {
+        task_usuario
+    });
+});
+
+router.post('/edit1/:id', async (req, res) => {
+    const { id } = req.params; 
+    const task = await Usu.updateMany({ _id: id }, req.body);
+    res.redirect('/admin');
 });
 
 router.get('/delete/:id', async (req, res) => {
@@ -93,5 +119,11 @@ router.get('/delete/:id', async (req, res) => {
     await Task.remove({ _id: id });
     res.redirect('/');
 });
+router.get('/delete1/:id', async (req, res) => {
+    const { id } = req.params;
+    await Usu.remove({ _id: id });
+    res.redirect('/admin');
+});
+
 
 module.exports = router;
